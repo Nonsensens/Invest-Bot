d446ad84385ad41de43be25c67c268a76f394bf5
Nonsense <66749680+Nonsensens@users.noreply.github.com>
2022-12-11 14:45:34 +0300

    Update create_bot.py

--- app/bot/create_bot.py
+++ app/bot/create_bot.py
@@ -2,5 +2,5 @@ from aiogram import Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-bot = Bot(token='880296384:AAEn2DlInZtFsN3oaw6aCYRDDJ8ZG43MtAk')
+bot = Bot(token={GITHUB-TOKEN})
 storage = MemoryStorage()
